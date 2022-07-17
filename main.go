4e2e8975051856eec51ebac1bc3fa7580309c5ce
ThomasDescroix <descroix.thomas@gmail.com>
2022-07-17 16:45:50 +0200

    Channels

--- main.go
+++ main.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 type Todo struct {
 	Id        int
@@ -8,24 +12,44 @@ type Todo struct {
 	Completed bool
 }
 
-type Toggleable interface {
-	toggle()
-}
-
-func (t *Todo) toggle() {
-	t.Completed = !t.Completed
-}
-
-func toggleable(t Toggleable) {
-	t.toggle()
-}
-
 func main() {
-	// map and struct
-	a := map[string]string{"name": "John", "surname": "Doe"}
-	a["name"] = "Jane"
-	b := Todo{1, "Buy milk", false}
-	b.toggle()
-	toggleable(&b)
-	fmt.Println(a, b)
+	// Chanel
+	ch := make(chan string)
+	errCh := make(chan error)
+	go func() {
+		title, err := fetchTodoTitle()
+		if err != nil {
+			errCh <- err
+			return
+		}
+		ch <- title
+		return
+	}()
+	fmt.Println("Hello, World!")
+	defer close(ch)
+	defer close(errCh)
+	select {
+	case err := <-errCh:
+		panic(err)
+	case title := <-ch:
+		fmt.Println(title)
+	}
+}
+
+func fetchTodoTitle() (string, error) {
+	r, err := http.Get("https://jsonplaceholder.typicode.com/todos?_limit=5")
+	if err != nil {
+		return "", fmt.Errorf("error fetching todos: %s", err)
+	}
+
+	defer r.Body.Close()
+	var todos []Todo
+	err = json.NewDecoder(r.Body).Decode(&todos)
+	if err != nil {
+		return "", fmt.Errorf("error decoding todos: %s", err)
+	}
+	if len(todos) > 0 {
+		return todos[0].Title, nil
+	}
+	return "", fmt.Errorf("no todos found")
 }
