3d00d5497d7279891fb8a531f141401b8984f5d5
ThomasDescroix <descroix.thomas@gmail.com>
2022-07-17 16:44:06 +0200

    Map, Struct and Interface

--- main.go
+++ main.go
@@ -2,10 +2,30 @@ package main
 
 import "fmt"
 
+type Todo struct {
+	Id        int
+	Title     string
+	Completed bool
+}
+
+type Toggleable interface {
+	toggle()
+}
+
+func (t *Todo) toggle() {
+	t.Completed = !t.Completed
+}
+
+func toggleable(t Toggleable) {
+	t.toggle()
+}
+
 func main() {
-	// Default values
-	var a int
-	var b string
-	var c []int
-	fmt.Printf("%#v %#v %#v\n", a, b, c)
+	// map and struct
+	a := map[string]string{"name": "John", "surname": "Doe"}
+	a["name"] = "Jane"
+	b := Todo{1, "Buy milk", false}
+	b.toggle()
+	toggleable(&b)
+	fmt.Println(a, b)
 }
